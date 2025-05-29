33901b1e3947a0cb9e4151b4dafafbce2ed956bf
Kacper Wach <kwach@aptean.com>
2025-05-29 13:32:55 +0200

    git init with ready backend

---
+++ front-end/src/main.jsx
@@ -0,0 +1,19 @@
+import "./styles/theme.css";
+import "./styles/globals.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Layout } from "./components/Layout/Layout";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router}></RouterProvider>
+    </React.StrictMode>
+);
